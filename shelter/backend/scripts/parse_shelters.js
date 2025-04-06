b4faee53b31b108f4ce3f52064f096f520fecd02
jimyung <sjm77773@naver.com>
2025-04-06 18:07:05 +0900

    대피시설 위치 정보 json파일  정제:DB에 들어갈 수 있도록 가공

--- shelter/backend/scripts/parse_shelters.js
+++ shelter/backend/scripts/parse_shelters.js
@@ -3,14 +3,18 @@ const csv = require('csv-parser');
 
 const results = [];
 
-fs.createReadStream('./data/raw/chungbuk.csv') // 업로드한 경로
+fs.createReadStream('./data/raw/chungbuk.csv')
   .pipe(csv())
   .on('data', (row) => {
-    // 먼저 열 키 확인
-    console.log(row);
-    results.push(row);
+    results.push({
+      name: row['민방위 대피시설명'],
+      address: row['시설 주소'],
+      lat: parseFloat(row['위도']),
+      lng: parseFloat(row['경도']),
+      region: '충청북도'
+    });
   })
   .on('end', () => {
-    fs.writeFileSync('./data/processed/chungbuk_raw.json', JSON.stringify(results, null, 2));
-    console.log('✅ 변환 완료! chungbuk_raw.json 생성됨');
+    fs.writeFileSync('./data/processed/chungbuk.json', JSON.stringify(results, null, 2));
+    console.log('✅ 정제된 chungbuk.json 생성 완료!');
   });
