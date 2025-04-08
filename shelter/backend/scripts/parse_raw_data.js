74aa9041cb3ee22fd6298b8d9f01fc10ceb7915a
jimyung <sjm77773@naver.com>
2025-04-08 17:10:11 +0900

    세종특별자치시 민방위대피소 위치정보 원본파일 json화 #9

--- shelter/backend/scripts/parse_raw_data.js
+++ shelter/backend/scripts/parse_raw_data.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 
 const results = [];
 
-fs.createReadStream('./data/raw/daegu.csv') // 업로드한 경로
+fs.createReadStream('./data/raw/sejong.csv') // 업로드한 경로
   .pipe(csv())
   .on('data', (row) => {
     // 먼저 열 키 확인
@@ -11,6 +11,6 @@ fs.createReadStream('./data/raw/daegu.csv') // 업로드한 경로
     results.push(row);
   })
   .on('end', () => {
-    fs.writeFileSync('./data/processed/rawJSON/daegu_raw.json', JSON.stringify(results, null, 2));
-    console.log('✅ 변환 완료! daegu_raw.json 생성됨');
+    fs.writeFileSync('./data/processed/rawJSON/sejong_raw.json', JSON.stringify(results, null, 2));
+    console.log('✅ 변환 완료! sejong_raw.json 생성됨');
   });
