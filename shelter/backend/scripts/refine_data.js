78fda82da56b242c74a4adeaea7583023091e1d2
jimyung <sjm77773@naver.com>
2025-04-08 12:13:18 +0900

    대전광역시 민방위대피소 위치정보 정제하여 json화 #5

--- shelter/backend/scripts/refine_data.js
+++ shelter/backend/scripts/refine_data.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 
 const results = [];
 
-fs.createReadStream('./data/raw/busan.csv')
+fs.createReadStream('./data/raw/daejeon.csv')
   .pipe(csv())
   .on('data', (row) => {
     results.push({
@@ -11,10 +11,10 @@ fs.createReadStream('./data/raw/busan.csv')
       address: row['도로명전체주소'],
       lat: parseFloat(row['위도(EPSG4326)']),
       lng: parseFloat(row['경도(EPSG4326)']),
-      region: '부산광역시'
+      region: '대전광역시'
     });
   })
   .on('end', () => {
-    fs.writeFileSync('./data/processed/busan.json', JSON.stringify(results, null, 2));
-    console.log('✅ 정제된 busan.json 생성 완료!');
+    fs.writeFileSync('./data/processed/refinedJSON/daejeon.json', JSON.stringify(results, null, 2));
+    console.log('✅ 정제된 daejeon.json 생성 완료!');
   });
