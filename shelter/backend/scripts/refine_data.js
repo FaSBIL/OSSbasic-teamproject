ec32b89d305bc4853597fba7c78b6eea1e619742
jimyung <sjm77773@naver.com>
2025-04-08 13:50:51 +0900

    제주특별자치도 민방위대피소 위치정보 데이터  정제하여 json화 #6

--- shelter/backend/scripts/refine_data.js
+++ shelter/backend/scripts/refine_data.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 
 const results = [];
 
-fs.createReadStream('./data/raw/daejeon.csv')
+fs.createReadStream('./data/raw/jeju.csv')
   .pipe(csv())
   .on('data', (row) => {
     results.push({
@@ -11,10 +11,10 @@ fs.createReadStream('./data/raw/daejeon.csv')
       address: row['도로명전체주소'],
       lat: parseFloat(row['위도(EPSG4326)']),
       lng: parseFloat(row['경도(EPSG4326)']),
-      region: '대전광역시'
+      region: '제주특별자치도'
     });
   })
   .on('end', () => {
-    fs.writeFileSync('./data/processed/refinedJSON/daejeon.json', JSON.stringify(results, null, 2));
-    console.log('✅ 정제된 daejeon.json 생성 완료!');
+    fs.writeFileSync('./data/processed/refinedJSON/jeju.json', JSON.stringify(results, null, 2));
+    console.log('✅ 정제된 jeju.json 생성 완료!');
   });
