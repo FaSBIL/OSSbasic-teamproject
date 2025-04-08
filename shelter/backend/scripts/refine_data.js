0949d92d384a08922a2573398c8a1e5d65b75f9c
jimyung <sjm77773@naver.com>
2025-04-08 23:47:17 +0900

    강원특별자치도 민방위대피소 위치정보 데이터 정제하여 json화 #11

--- shelter/backend/scripts/refine_data.js
+++ shelter/backend/scripts/refine_data.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 
 const results = [];
 
-fs.createReadStream('./data/raw/sejong.csv')
+fs.createReadStream('./data/raw/gangwon.csv')
   .pipe(csv())
   .on('data', (row) => {
     results.push({
@@ -11,10 +11,10 @@ fs.createReadStream('./data/raw/sejong.csv')
       address: row['도로명전체주소'],
       lat: parseFloat(row['위도(EPSG4326)']),
       lng: parseFloat(row['경도(EPSG4326)']),
-      region: '세종특별자치시'
+      region: '강원특별자치도'
     });
   })
   .on('end', () => {
-    fs.writeFileSync('./data/processed/refinedJSON/sejong.json', JSON.stringify(results, null, 2));
-    console.log('✅ 정제된 sejong.json 생성 완료!');
+    fs.writeFileSync('./data/processed/refinedJSON/gangwon.json', JSON.stringify(results, null, 2));
+    console.log('✅ 정제된 gangwon.json 생성 완료!');
   });
