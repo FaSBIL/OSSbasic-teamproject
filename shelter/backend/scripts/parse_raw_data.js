70f31438d2a01b58ed5b28d9239a45cd6f62e274
jimyung <sjm77773@naver.com>
2025-04-09 11:25:15 +0900

    경상북도 민방위대피소 위치정보 원본데이터 json화 #20

--- shelter/backend/scripts/parse_raw_data.js
+++ shelter/backend/scripts/parse_raw_data.js
@@ -3,7 +3,7 @@ const csv = require('csv-parser');
 
 const results = [];
 
-fs.createReadStream('./data/raw/gyeongnam.csv') // 업로드한 경로
+fs.createReadStream('./data/raw/gyeongbuk.csv') // 업로드한 경로
   .pipe(csv())
   .on('data', (row) => {
     // 먼저 열 키 확인
@@ -11,6 +11,6 @@ fs.createReadStream('./data/raw/gyeongnam.csv') // 업로드한 경로
     results.push(row);
   })
   .on('end', () => {
-    fs.writeFileSync('./data/processed/rawJSON/gyeongnam_raw.json', JSON.stringify(results, null, 2));
-    console.log('✅ 변환 완료! gyeongnam_raw.json 생성됨');
+    fs.writeFileSync('./data/processed/rawJSON/gyeongbuk_raw.json', JSON.stringify(results, null, 2));
+    console.log('✅ 변환 완료! gyeongbuk_raw.json 생성됨');
   });
