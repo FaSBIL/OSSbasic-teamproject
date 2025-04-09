155c0e94e79cdb9937369f539bc64f47f1e46043
iamyuna01 <iamu.design01@gmail.com>
2025-04-09 15:16:27 +0900

    [add]지진 아이콘 추가#22

--- shelter/frontend/lib/component/icon/IconUtils.dart
+++ shelter/frontend/lib/component/icon/IconUtils.dart
@@ -6,6 +6,7 @@ class AppIcons {
   static const IconData location = Icons.dns_rounded;
   static const IconData history = Icons.watch_later_outlined;
   static const IconData tsunami = Icons.tsunami;
+  static const IconData earthquake = Icons.show_chart;
   static const IconData menu = Icons.menu_rounded;
   static const IconData arrowRight = Icons.arrow_back_ios_new_rounded;
   static const IconData arrowLeft = Icons.arrow_forward_ios_rounded;
