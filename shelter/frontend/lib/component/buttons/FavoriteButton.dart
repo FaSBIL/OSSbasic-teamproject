50a8667beceeafe3fd2a3108a28900643c190174
iamyuna01 <iamu.design01@gmail.com>
2025-04-09 18:21:03 +0900

    [add]FavoriteButton 디자인 컴포넌트 추가#23

---
+++ shelter/frontend/lib/component/buttons/FavoriteButton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../../theme/color.dart';
+import '../icon/customIcon.dart';
+import '../icon/iconUtils.dart';
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({
+    Key? key
+  }) : super(key: key);
+
+  @override
+  State<FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton>{
+  bool isFavorited = false;
+  
+  void _toggleFavorite(){
+    setState((){
+      isFavorited = !isFavorited;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomIcon(
+      iconData: isFavorited ? AppIcons.star : AppIcons.starRound,
+      color: AppColors.blue,
+      backgroundColor:  AppColors.paleBlue,
+      borderColor: AppColors.paleBlue,
+      onTap: _toggleFavorite,
+      isClickable: true,
+    );
+  }
+}
