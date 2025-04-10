6d791b8badcd5d72e7617b9122d76f3a64bacb6a
iamyuna01 <iamu.design01@gmail.com>
2025-04-10 20:28:23 +0900

    [fix]NotoSansKR 폰트 weight 설정 수정#34

--- shelter/frontend/lib/theme/typography.dart
+++ shelter/frontend/lib/theme/typography.dart
@@ -7,56 +7,56 @@ class AppTextStyles{
   static const TextStyle title = TextStyle(
     fontFamily: fontFamily,
     fontSize: 18,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.black,
   );
 
   static const TextStyle subtitle = TextStyle(
     fontFamily: fontFamily,
     fontSize: 16,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.black,
   );
 
   static const TextStyle body = TextStyle(
     fontFamily: fontFamily,
     fontSize: 14,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.black,
   );
 
   static const TextStyle bodyGray = TextStyle(
     fontFamily: fontFamily,
     fontSize: 14,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.gray,
   );
 
   static const TextStyle caption = TextStyle(
     fontFamily: fontFamily,
     fontSize: 12,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.black,
   );
 
   static const TextStyle captionGray = TextStyle(
     fontFamily: fontFamily,
     fontSize: 12,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.gray,
   );
 
   static const TextStyle small = TextStyle(
     fontFamily: fontFamily,
     fontSize: 10,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.black,
   );
 
   static const TextStyle smallGray = TextStyle(
     fontFamily: fontFamily,
     fontSize: 10,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.gray,
   );
 }
