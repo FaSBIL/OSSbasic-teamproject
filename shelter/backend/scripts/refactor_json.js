ab69134d03e2d7216f7a60994f579927a1d1046c
jimyung <sjm77773@naver.com>
2025-04-10 15:13:11 +0900

    page 5: 응답받은 json파일에서 순수데이터 추출 #29

--- shelter/backend/scripts/refactor_json.js
+++ shelter/backend/scripts/refactor_json.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 // 원본 파일 경로
-const inputPath = './data/earthquake/RawJSON/response4.json';
+const inputPath = './data/earthquake/RawJSON/response5.json';
 // 저장할 파일 경로
-const outputPath = './data/earthquake/RawJSON/earthquake_shelters4.json';
+const outputPath = './data/earthquake/RawJSON/earthquake_shelters5.json';
 
 // JSON 읽기
 const raw = fs.readFileSync(inputPath, 'utf8');
@@ -14,9 +14,9 @@ const parsed = JSON.parse(raw);
 const shelters = parsed.body;
 
 if (!Array.isArray(shelters)) {
-  throw new Error('❌ response4.json 안의 body가 배열이 아닙니다.');
+  throw new Error('❌ response5.json 안의 body가 배열이 아닙니다.');
 }
 
 // 저장
 fs.writeFileSync(outputPath, JSON.stringify(shelters, null, 2), 'utf8');
-console.log(`✅ 총 ${shelters.length}개의 대피소 데이터를 earthquake_shelters4.json에 저장했습니다.`);
+console.log(`✅ 총 ${shelters.length}개의 대피소 데이터를 earthquake_shelters5.json에 저장했습니다.`);
