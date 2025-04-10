284b82c56673bd49579335f85dd5e88ed2215d43
iamyuna01 <iamu.design01@gmail.com>
2025-04-10 17:48:56 +0900

    [add]ThemeModeItem 디자인  추가 & 공통 레이아웃 BaseListItem 추가#30

---
+++ shelter/frontend/lib/component/list/BaseListItem.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../../theme/color.dart';
+
+class BaseListItem extends StatelessWidget {
+  final Widget title;
+  final Widget? subtitle;
+  final Widget? leading;
+  final Widget? trailing;
+  final VoidCallback? onTap;
+  final EdgeInsetsGeometry padding;
+
+  const BaseListItem({
+    Key? key,
+    required this.title,
+    this.subtitle,
+    this.leading,
+    this.trailing,
+    this.onTap,
+    this.padding = const EdgeInsets.symmetric(vertical: 18, horizontal: 4),
+  })  : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: onTap,
+          splashFactory: NoSplash.splashFactory,
+          highlightColor: Colors.transparent,
+          child: Padding(
+            padding: padding,
+            child: Row(
+              crossAxisAlignment: subtitle != null
+                  ? CrossAxisAlignment.start
+                  : CrossAxisAlignment.center,
+              children: [
+                if (leading != null) ...[
+                  leading!,
+                  const SizedBox(width: 12),
+                ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      title,
+                      if (subtitle != null) ...[
+                        const SizedBox(height: 8),
+                        subtitle!,
+                      ],
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 12),
+                trailing ?? const SizedBox(width: 24, height: 24),
+              ],
+            ),
+          ),
+        ),
+        const Divider(height: 1, thickness: 1, color: AppColors.lightGray),
+      ],
+    );
+  }
+}
