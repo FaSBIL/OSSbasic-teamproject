10075f617c8bfe4da6da60649f4db7a2f019e555
iamyuna01 <iamu.design01@gmail.com>
2025-05-02 01:57:06 +0900

    [fix]아이콘 정의 코드 수정#59

--- shelter/frontend/lib/component/icon/IconUtils.dart
+++ shelter/frontend/lib/component/icon/IconUtils.dart
@@ -13,5 +13,5 @@ class AppIcons {
   static const IconData check = Icons.check;
   static const IconData destination = Icons.location_on_rounded;
   static const IconData gpsFixed = Icons.gps_fixed_rounded;
-  static const IconData shelterResult = Icons.Icons.flag_circle_rounded;
+  static const IconData shelterResult = Icons.flag_circle_rounded;
 }
