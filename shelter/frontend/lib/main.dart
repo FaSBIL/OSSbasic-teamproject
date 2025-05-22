41a5ca7dd5c94b3e7a92e97113c258c91405c4b7
iamyuna01 <iamu.design01@gmail.com>
2025-05-22 16:53:16 +0900

    [fix]테마 모드 기능 연동(Provider 및 SharedPreferences 적용) #84

--- shelter/frontend/lib/main.dart
+++ shelter/frontend/lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:shelter/screens/settings/SettingsMainScreens.dart';
-import 'screens/test.dart';
-import 'routes/AppRoutes.dart';
-import 'screens/location_screen.dart';
-import 'screens/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:shelter/theme/theme_manager.dart';
 
-void main() {
+import 'routes/AppRoutes.dart';
+import 'package:shelter/screens/settings/SettingsMainScreens.dart';
+import 'package:shelter/screens/home_screen.dart';
+
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+
+  final themeManager = ThemeManager();
+  await themeManager.loadTheme(); //SharedPreferences에서 테마 불러오기
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => themeManager,
+      child: const MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,14 +25,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '대피소 앱', // 앱의 기본 제목 (iOS나 일부 안드로이드에서 사용됨)
-      theme: ThemeData(
-        fontFamily: 'NotoSansKR', // 앱 전체에서 사용할 기본 글꼴
-      ),
+    final themeManager = Provider.of<ThemeManager>(context);
 
+    return MaterialApp(
+      title: '대피소 앱',
+      theme: ThemeData.light().copyWith(
+      // 앱의 기본 제목 (iOS나 일부 안드로이드에서 사용됨)
+        textTheme: ThemeData.light().textTheme.apply(
+          fontFamily: 'NotoSansKR',
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+      // 앱의 기본 제목 (iOS나 일부 안드로이드에서 사용됨)
+        textTheme: ThemeData.dark().textTheme.apply(
+          fontFamily: 'NotoSansKR',
+        ),
+      ),
+      themeMode: themeManager.themeMode, // 테마 적용
       routes: AppRoutes.routes,
-      home: HomeScreen(), // 앱 실행 시 처음 보여줄 화면
+      //home: HomeScreen(), // 앱 실행 시 처음 보여줄 화면
+      home: SettingsMainScreen(),
       debugShowCheckedModeBanner: false, // 오른쪽 상단 디버그 배너 제거
     );
   }
