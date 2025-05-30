a943272c576f9e064ad2a39dff45961d50cb7473
iamyuna01 <iamu.design01@gmail.com>
2025-05-30 23:47:03 +0900

    [fix]음성 안내 설정 화면에서 iOS 기기일 경우 음소거 토글을 숨기고 관련 안내 문구를 추가#106

--- shelter/frontend/lib/screens/settings/VoiceGuideScreen.dart
+++ shelter/frontend/lib/screens/settings/VoiceGuideScreen.dart
@@ -4,6 +4,7 @@ import '../../theme/typography.dart';
 import '../../component/settingItem/ToggleSwitch.dart';
 import '../../component/settingItem/VolumeSlider.dart';
 import '../../controllers/tts_controller.dart';
+import 'dart:io';
 
 class VoiceGuideScreen extends StatefulWidget {
   const VoiceGuideScreen({super.key});
@@ -70,26 +71,32 @@ class _VoiceGuideScreenState extends State<VoiceGuideScreen> {
             ),
             const SizedBox(height: 20),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Expanded(
-                  child: Text(
-                    '음소거 시에도 음성 경보를 켜기',
-                    style: AppTextStyles.subtitle,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                ToggleSwitch(
-                  isOn: isMutedModeOn,
-                  onChanged: (value) async {
-                    await _ttsController.setAllowVoiceInSilentMode(value);
-                    setState(() => isMutedModeOn = value);
-                  }
-                ),
-              ],
-            ),
-            const SizedBox(height: 32),
+            Platform.isAndroid
+              ? Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Expanded(
+                          child: Text(
+                            '음소거 시에도 음성 경보를 켜기',
+                            style: AppTextStyles.subtitle,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        ToggleSwitch(
+                          isOn: isMutedModeOn,
+                          onChanged: (value) async {
+                            await _ttsController.setAllowVoiceInSilentMode(value);
+                            setState(() => isMutedModeOn = value);
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 32),
+                  ],
+                )
+              : const SizedBox.shrink(),
 
             const Text('음량 조절', style: AppTextStyles.subtitle),
             const SizedBox(height: 12),
@@ -97,11 +104,19 @@ class _VoiceGuideScreenState extends State<VoiceGuideScreen> {
             VolumeSlider(
               value: volume,
               onChanged: (value) async {
+                if(!isVoiceGuideOn) return;
                 setState(() => volume = value);
                 await _ttsController.setVolume(value);
                 await _ttsController.speak("안내를 시작합니다.");
               },
               enabled: isVoiceGuideOn,
+            ),
+            const SizedBox(height: 20),
+
+            const SizedBox(height: 8),
+            const Text(
+              '일부 기기에서는 음소거 상태와 관계없이 음성이 항상 재생될 수 있습니다.',
+              style: AppTextStyles.bodyGray
             ),
           ],
         ),
