3860d04402c99f736b89798580767cc87894a810
iamyuna01 <iamu.design01@gmail.com>
2025-05-31 13:06:24 +0900

    [delate]현재 위치 메뉴 link 삭제#109

--- shelter/frontend/lib/screens/settings/SettingsMainScreens.dart
+++ shelter/frontend/lib/screens/settings/SettingsMainScreens.dart
@@ -41,17 +41,6 @@ class SettingsMainScreen extends StatelessWidget {
                         AppRoutes.backgroundActivity,
                       ),
                 ),
-                SettingsNavItem(
-                  label: '위치 서비스',
-                  onTap:
-                      () => Navigator.pushNamed(
-                        context,
-                        AppRoutes.location,
-                      ), // 위치 화면으로 이동
-                ),
-
-                const SizedBox(height: 8),
-
                 const ConfigItem(label: 'GPS', value: 'ON'),
                 const ConfigItem(label: '앱 정보', value: 'version 1.0'),
               ],
